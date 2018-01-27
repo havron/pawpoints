88a35d7a5838e8cb6342cf7e695b90ecd5d6a702
Samuel Havron <sgh7cc@virginia.edu>
2018-01-27 15:49:39 -0500

    added Facebook auth

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+	def show
+		@signed_in = Users.find(params[:id])
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+    
+  root 'welcome#index'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :pets#, only: [:index, :create, :destroy, :update]
 end
