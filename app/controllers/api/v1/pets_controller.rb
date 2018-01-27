ce24f23ebe60d236dff10ee38d4dec4cb2403618
Shannon Joyner <shannonjoyner@dhcp-hol-849.wifi-visitor.cit.cornell.edu>
2018-01-27 14:07:30 -0500

    Pets model

---
+++ app/controllers/api/v1/pets_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::PetsController < Api::V1::BaseController
+  def index
+    respond_with Pet.all
+  end
+
+  def create
+    respond_with :api, :v1, Pet.create(pet_params)
+  end
+
+  def destroy
+    respond_with Pet.destroy(params[:id])
+  end
+
+  def update
+    pet = Pet.find(params["id"])
+    pet.update_attributes(pet_params)
+    respond_with pet, json: pet
+  end 
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:id, :name, :description)
+  end
+end
