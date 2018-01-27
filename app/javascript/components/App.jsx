a6ed834e06a3ffbefdc73d5674fd6d71486fdfab
Shannon Joyner <shannonjoyner@dhcp-hol-849.wifi-visitor.cit.cornell.edu>
2018-01-27 16:19:09 -0500

    App: Add Option icons
    These are the options to change setting, get information, etc.

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Profile from './Profile.jsx'
+import Options from './Options.jsx'
+
+require ('./stylesheets/app.scss');
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
+        <Options/>
         <Profile/>
       </div>
     );
