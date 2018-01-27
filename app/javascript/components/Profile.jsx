3b5dbba3e22bdc7bab3cc9dab126770003f2d5ad
Shannon Joyner <shannonjoyner@dhcp-hol-849.wifi-visitor.cit.cornell.edu>
2018-01-27 15:37:59 -0500

    Move javascript into app folder

--- app/javascript/components/Profile.jsx
+++ app/javascript/components/Profile.jsx
@@ -6,6 +6,7 @@ export default class Profile extends React.Component {
     return (
       <div>
         <h1>Profile</h1>
+        <h2>Puss in Boots</h2>
         <img className="profilePic" src="http://assets.nydailynews.com/polopoly_fs/1.968084.1319855101!/img/httpImage/image.jpg_gen/derivatives/article_750/alg-puss-boots-jpg.jpg"/>
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
+  template: './app/javascript/index.html',
   filename: 'index.html',
   inject: 'body'
 })
 module.exports = {
-  entry: './client/index.js',
+  entry: './app/javascript/index.js',
   output: {
     path: path.resolve('dist'),
     filename: 'index_bundle.js'
